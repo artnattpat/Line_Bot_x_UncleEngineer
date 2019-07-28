c286e8936b02a132c4eeed85c65c129ad12805ea
artnattpat <52275214+artnattpat@users.noreply.github.com>
2019-07-28 14:43:52 +0700

    Update Config.py

--- Project/Config.py
+++ Project/Config.py
@@ -2,10 +2,10 @@
 
 
 
-Channel_secret = "da180eac5e665afbfc0a40232d524049"
+Channel_secret = "c433556039b630918058626c200a10b7"
 
 
-Channel_access_token = "tdW2cT7Rxu4MWnV3Z9nEy9EWIbsy3fFX8De+lX6xIiL5OU0YYLr5BugLvT3qZVNEtK9itMtnFcxkh0MFbisLCtmlHXWJHG43919vEHti6RnWPKJ6Cs3WfouwVwGntNL/zXhRSv3OTiefzpzXjUwsKQdB04t89/1O/w1cDnyilFU="
+Channel_access_token = "iMQTYaxWQkUcijJ5Zc7EHM6w4dDk+MJFZx9xagdntDX91Y11LnU/Zwyf6XaRwj9WdTEyf5KishrU9wgCJ2zK6cj4CBK+AHD/x64PI2qUlGub8Xv2t1gCslUeAFMLzBpUKwzivK7ZK/hOFaOdwCSnXwdB04t89/1O/w1cDnyilFU="
 
 
-Basic_ID = "@108lsrxj"
+Basic_ID = "@144xcbik"
